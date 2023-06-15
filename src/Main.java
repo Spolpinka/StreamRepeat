fd645c9b05a154dae4f9d4b71eb0a8d0e88cc669
Spolpinka <a421243266@gmail.com>
2023-06-15 06:41:34 +0300

    оба задания

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,22 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
+    public static void main(String[] args) {
+        printCountOfEven(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
 
+    //задание 1
+    public static<T> void findMinMax(Stream<? extends T> stream, Comparator<? super T> order, BiConsumer<? super T, ? super T> minMaxConsumer) {
+        T min = stream.min(order).orElse(null);
+        T max = stream.max(order).orElse(null);
+
+        minMaxConsumer.accept(min, max);
+    }
+
+    //Задание 2 - вывод четных чисел из потока в консоль
+    public static void printCountOfEven(List<Integer> list) {
+        list.stream()
+                .filter(i -> i % 2 == 0)
+                .forEach(System.out::println);
+    }
 }
